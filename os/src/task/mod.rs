753ef47a3ffc580844feb8eb0dc7292c6b50dc21
Jensonal <42882164+Jensonal@users.noreply.github.com>
2024-11-06 11:37:07 +0800

    Jensonal
    ch3 达成

--- os/src/task/mod.rs
+++ os/src/task/mod.rs
@@ -14,9 +14,10 @@ mod switch;
 #[allow(clippy::module_inception)]
 mod task;
 
-use crate::config::MAX_APP_NUM;
+use crate::config::{MAX_APP_NUM, MAX_SYSCALL_NUM};
 use crate::loader::{get_num_app, init_app_cx};
 use crate::sync::UPSafeCell;
+use crate::timer::get_time_ms;
 use lazy_static::*;
 use switch::__switch;
 pub use task::{TaskControlBlock, TaskStatus};
@@ -54,6 +55,8 @@ lazy_static! {
         let mut tasks = [TaskControlBlock {
             task_cx: TaskContext::zero_init(),
             task_status: TaskStatus::UnInit,
+            task_syscall_times: [0; MAX_SYSCALL_NUM], // 初始化系统调用次数
+            task_time: 0,
         }; MAX_APP_NUM];
         for (i, task) in tasks.iter_mut().enumerate() {
             task.task_cx = TaskContext::goto_restore(init_app_cx(i));
@@ -80,6 +83,7 @@ impl TaskManager {
         let mut inner = self.inner.exclusive_access();
         let task0 = &mut inner.tasks[0];
         task0.task_status = TaskStatus::Running;
+        task0.task_time = get_time_ms();
         let next_task_cx_ptr = &task0.task_cx as *const TaskContext;
         drop(inner);
         let mut _unused = TaskContext::zero_init();
@@ -122,6 +126,9 @@ impl TaskManager {
             let mut inner = self.inner.exclusive_access();
             let current = inner.current_task;
             inner.tasks[next].task_status = TaskStatus::Running;
+            let current_time = get_time_ms();
+            inner.tasks[current].task_time = current_time - inner.tasks[current].task_time;
+            inner.tasks[next].task_time = current_time;
             inner.current_task = next;
             let current_task_cx_ptr = &mut inner.tasks[current].task_cx as *mut TaskContext;
             let next_task_cx_ptr = &inner.tasks[next].task_cx as *const TaskContext;
@@ -134,6 +141,25 @@ impl TaskManager {
         } else {
             panic!("All applications completed!");
         }
+    }
+    fn current_task_status(&self) -> TaskStatus {
+        let inner = self.inner.exclusive_access();
+        inner.tasks[inner.current_task].task_status
+    }
+    fn current_task_syscall_times(&self) -> [u32; MAX_SYSCALL_NUM] {
+        let inner = self.inner.exclusive_access();
+        inner.tasks[inner.current_task].task_syscall_times
+    }
+    fn increment_current_task_syscall(&self, syscall_index: usize) {
+        let mut inner = self.inner.exclusive_access();
+        let current = inner.current_task;
+        inner.tasks[current].task_syscall_times[syscall_index] += 1;
+    }
+    fn current_task_time(&self) -> usize {
+        let inner = self.inner.exclusive_access();
+        inner.tasks[inner.current_task].task_time
+    //     let inner = TASK_MANAGER.inner.exclusive_access();
+    // inner.tasks[inner.current_task].task_time
     }
 }
 
@@ -168,4 +194,20 @@ pub fn suspend_current_and_run_next() {
 pub fn exit_current_and_run_next() {
     mark_current_exited();
     run_next_task();
+}
+/// get current task status
+pub fn get_current_task_status() -> TaskStatus {
+    TASK_MANAGER.current_task_status()
+}
+/// get current task syscall_times
+pub fn get_current_task_syscall_times() -> [u32; MAX_SYSCALL_NUM] {
+    TASK_MANAGER.current_task_syscall_times()
+}
+/// get current task time
+pub fn get_current_task_time() -> usize {
+    TASK_MANAGER.current_task_time()
+}
+/// get current task increment_current_task_syscall
+pub fn increment_current_task_syscall(syscall_index: usize) {
+    TASK_MANAGER.increment_current_task_syscall(syscall_index);
 }
